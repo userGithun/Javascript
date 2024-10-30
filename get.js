756cd0d4e8d7afaf22602b1eb3a76def6aeacc37
userGithun <chacha45454@gmail.com>
2024-10-30 18:28:20 +0530

    first commit

---
+++ get.js
@@ -0,0 +1,7 @@
+// data = document.getElementById("a").innerHTML
+// data = document.getElementById("a").innerText
+// data = document.getElementById("a").getAttribute("style");
+// data = document.getElementById("a").getAttributeNode("style");
+d;ata = document.getElementById("a").attributes[0]
+
+console.log(data);
---
+++ set.js
@@ -0,0 +1,11 @@
+document.getElementById('a').innerHTML=("<center><h2>hello pninfosys gwalior</h2></center>");
+
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+
+document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML="<center><h1>gwalior</h1></center>";
+
+//querySelector
+// document.querySelector('#a').innerHTML="<center><h2>PNINFOSYS GWALIOR 1234</h2></center>";
+
+document.querySelector('.c').innerHTML="<h1>hwllo gwalior 1234566</h1>"
